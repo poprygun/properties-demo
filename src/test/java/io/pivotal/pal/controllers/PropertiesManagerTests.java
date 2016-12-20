6be727bb4fb2a185c5c7fc91195e26e0b353077f
Alex Shumilov <ashumilov@pivotal.io>
2016-12-20 17:57:11 -0500

    cleanup

--- src/test/java/io/pivotal/pal/controllers/PropertiesManagerTests.java
+++ src/test/java/io/pivotal/pal/controllers/PropertiesManagerTests.java
@@ -11,16 +11,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PropertiesManager.class)
